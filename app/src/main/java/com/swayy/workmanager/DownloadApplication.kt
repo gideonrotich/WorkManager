140366f4a39363b11f98cee1eb5b3ec5367d57ba
Giddyrotich433 <Giddyrotich433@gmail.com>
2023-02-10 12:26:02 +0300

    Initial commit

---
+++ app/src/main/java/com/swayy/workmanager/DownloadApplication.kt
@@ -0,0 +1,27 @@
+package com.swayy.workmanager
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class DownloadApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "download_channel",
+                "file download",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+    }
+
+}
